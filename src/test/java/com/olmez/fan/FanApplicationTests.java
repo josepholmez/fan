d8c61dde6c46ce3879f9e2b95622b3f31775acd9
JosephO <joe.olmez@gmail.com>
2023-03-17 13:29:18 -0400

    initial

---
+++ src/test/java/com/olmez/fan/FanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olmez.fan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
